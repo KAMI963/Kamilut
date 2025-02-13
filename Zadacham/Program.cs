d4c265d88922dfb9ac8e9863af84ee45f9a722e1
Студент <@PC-6508>
2025-02-13 13:18:25 +0300

    Добавили задачку

---
+++ Zadacham/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Random random = new Random();
+
+
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(-10, 11); 
+            }
+
+            Console.WriteLine("Сгенерированный массив: " + string.Join(", ", numbers));
+
+            int sumPositive = 0;
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    sumPositive += number;
+                }
+            }
+
+            Console.WriteLine("Сумма положительных чисел: " + sumPositive);
+        }
+    }
+}
