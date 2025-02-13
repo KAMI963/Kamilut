bc09133347485ef2dce7c8e449fd386a894fe158
Студент <@PC-6508>
2025-02-13 13:13:28 +0300

    Изменен код на задачу

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -10,6 +10,24 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
+                Random random = new Random();
+                int[] numbers = new int[10];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = random.Next(-10, 11);
+                }
+                Console.WriteLine("Сгенерированный массив: " + string.Join(", ", numbers));
+                int sumPositive = 0;
+                foreach (int number in numbers)
+                {
+                    if (number > 0)
+                    {
+                        sumPositive += number;
+                    }
+                }
+                Console.WriteLine("Сумма положительных чисел: " + sumPositive);
+                Console.ReadLine();
+            
         }
     }
 }
